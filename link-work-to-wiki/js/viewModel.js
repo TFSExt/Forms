6f3eb0c5efcc634645e9fff15f912980a3baf8d1
Anton Boyko <me@boykoant.pro>
2018-06-29 02:06:55 +0300

    migrate all code to knockoutjs

---
+++ link-work-to-wiki/js/viewModel.js
@@ -0,0 +1,100 @@
+/**
+ *
+ * @param model
+ * @param artifactsServices
+ * @param artifactsConstants
+ * @returns {{error: KnockoutObservable<any>, view: KnockoutObservable<any>, links: KnockoutObservableArray<any>}}
+ */
+function getViewModel(model, artifactsServices, artifactsConstants) {
+    var con = {
+        wait: "wait",
+        links: "links",
+        error: "error"
+    };
+
+    var viewModel = {
+        error: ko.observable(""),
+        view: ko.observable(""),
+
+        links: ko.observableArray([])
+    };
+
+    viewModel.isWaitVisible = ko.pureComputed(function () {
+        return viewModel.view() === con.wait;
+    });
+    viewModel.showWait = function () {
+        viewModel.view(con.wait);
+    };
+
+    viewModel.isLinksVisible = ko.pureComputed(function () {
+        return viewModel.view() === con.links;
+    });
+    viewModel.showLinks = function () {
+        viewModel.view(con.links);
+    };
+
+    viewModel.isErrorVisible = ko.pureComputed(function () {
+        return viewModel.view() === con.error;
+    });
+    viewModel.showError = function () {
+        viewModel.view(con.error);
+    };
+
+    viewModel.clear = function () {
+        viewModel.showWait();
+        viewModel.removeAll();
+        viewModel.showLinks();
+    };
+
+    viewModel.reload = function () {
+        viewModel.showWait();
+        model.getCurrentWorkItemRelations()
+            .then(function (links) {
+                links
+                    // filter for valid links only
+                    .filter(function (link) {
+                        return link.url.startsWith("vstfs:///");
+                    })
+                    // get link data
+                    .map(function (link) {
+                        return artifactsServices.LinkingUtilities.decodeUri(link.url);
+                    })
+                    // filter for wiki pages only
+                    .filter(function(artifact) {
+                        return artifact.type === artifactsConstants.ArtifactTypeNames.WikiPage;
+                    })
+                    // get UI link items
+                    .map(function(artifact) {
+                        var segments = artifact.id.split("/");
+                        var pagePath = [];
+                        for (var i = 2; i < segments.length; i++) {
+                            pagePath.push(segments[i]);
+                        }
+
+                        var url = [
+                            model.context.account.uri,
+                            model.context.project.name,
+                            "/_wiki/wikis/",
+                            segments[1] + "?pagePath=" + pagePath.join("/")
+                        ].join("");
+
+                        return {
+                            url: url,
+                            text: pagePath[pagePath.length - 1]
+                        };
+                    })
+                    // bind to UI
+                    .forEach(function (linkItem) {
+                        viewModel.links.push(linkItem);
+                    });
+
+                viewModel.showLinks();
+            },
+            function (error) {
+                viewModel.error(error.message);
+                viewModel.showError();
+            });
+    };
+
+    return viewModel;
+}
